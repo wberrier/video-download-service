c8c649db232146ef97157c69e3011235a5fcc630
Wade Berrier <wberrier@gmail.com>
2020-10-12 16:13:18 -0600

    add index page

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 
 use warp::Filter;
 
+async fn display_index() -> Result<impl warp::Reply, warp::Rejection> {
+
+        Ok(warp::reply::with_status(
+            "Welcome to the downloader service",
+            warp::http::StatusCode::CREATED,
+        ))
+}
+
 #[tokio::main]
 async fn main() {
-    // GET /hello => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello")
-        .map(|| format!("Hello, World!"));
 
-    warp::serve(hello)
+    // GET / => 200 OK with index body
+    let main_page = warp::get()
+        .and(warp::path::end())
+        .and_then(display_index);
+
+    warp::serve(main_page)
         .run(([0, 0, 0, 0], 8080))
         .await;
 }
